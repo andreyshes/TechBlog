7106385a2a746e62b8af39ce51dda8b73094392d
Andrey <ashesto121@gmail.com>
2023-05-31 11:08:33 -0700

    javascript for pages

---
+++ public/js/edit-blog.js
@@ -0,0 +1,29 @@
+async function editFormHandler(event) {
+	event.preventDefault();
+
+	const title = document.querySelector('input[name="blog-title"]').value;
+	const content = document.querySelector('textarea[name="blog-content"]').value;
+	const id = window.location.toString().split("/")[
+		window.location.toString().split("/").length - 1
+	];
+
+	const response = await fetch(`/api/blogs/${id}`, {
+		method: "PUT",
+		body: JSON.stringify({
+			title,
+			content,
+		}),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+	if (response.ok) {
+		document.location.replace("/dashboard/");
+	} else {
+		alert(response.statusText);
+	}
+}
+
+document
+	.querySelector(".edit-blog-form")
+	.addEventListener("submit", editFormHandler);
